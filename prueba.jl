fe7285a8a7c42a37403ba1ba5e23687d95c80457
Nicolás Díaz <nicolasdiazn@ciencias.unam.mx>
2016-09-12 21:49:25 -0500

    Cambio para activar Travis

--- prueba.jl
+++ prueba.jl
@@ -3,8 +3,6 @@ function Hermitian(n)
     a=rand(n,n)+1im*rand(n,n)
     #Posteriormente se utiliza la función triu(a,1) para obtener la matriz triangular superior a partir de la entrada 1
     b=triu(a,1)
-    #Se construye la matriz M, la cual toma b (matriz triangular superior)+la transpuesta conjugada de b 
-    #(es decir una matriz trian inferior) y luego le suma una diagonal rándom. 
     M=b+transpose(conj(b))+Diagonal(rand(n,n))
     return M
 end
